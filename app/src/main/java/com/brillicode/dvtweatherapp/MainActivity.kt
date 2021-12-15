8825c39958f9b630edcb9893fe00c29533581898
ksesotlo@ebay.com <ksesotlo@ebay.com>
2021-12-15 04:28:16 +0200

    Add Dexter and check permissions

--- app/src/main/java/com/brillicode/dvtweatherapp/MainActivity.kt
+++ app/src/main/java/com/brillicode/dvtweatherapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.brillicode.dvtweatherapp
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +11,12 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.brillicode.dvtweatherapp.databinding.ActivityMainBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        validatePermission()
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +42,35 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun validatePermission() {
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?, token: PermissionToken?) {
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle(R.string.location_permission_rationale_title)
+                        .setMessage(R.string.location_permission_rationale_message)
+                        .setNegativeButton(android.R.string.cancel) { dialogInterface, _ ->
+                            dialogInterface.dismiss()
+                            token?.cancelPermissionRequest()
+                        }
+                        .setPositiveButton(android.R.string.ok) { dialogInterface, _ ->
+                            dialogInterface.dismiss()
+                            token?.continuePermissionRequest()
+                        }
+                        .show()
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Toast.makeText(this@MainActivity, R.string.location_permission_denied_message, Toast.LENGTH_LONG).show()
+                }
+            }
+            ).check()
     }
 }

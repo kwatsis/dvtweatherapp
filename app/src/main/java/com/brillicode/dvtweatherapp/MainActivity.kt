fd1729b1ac0626c641b10aa3f202c189cd947fc4
ksesotlo@ebay.com <ksesotlo@ebay.com>
2021-12-21 06:23:15 +0200

    Add fonts and remove actionbar

--- app/src/main/java/com/brillicode/dvtweatherapp/MainActivity.kt
+++ app/src/main/java/com/brillicode/dvtweatherapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.brillicode.dvtweatherapp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.brillicode.dvtweatherapp.base.BaseActivity
 import com.brillicode.dvtweatherapp.databinding.ActivityMainBinding
@@ -20,16 +18,8 @@ class MainActivity : BaseActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a setvalidatePermission() of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_favorites
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 

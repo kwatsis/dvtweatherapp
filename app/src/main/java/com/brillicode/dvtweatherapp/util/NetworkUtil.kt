47bdd9b50a63ea411e997b5302bca8d22514335a
ksesotlo@ebay.com <ksesotlo@ebay.com>
2021-12-22 17:45:40 +0200

    Include temperature ranges to the home screen

---
+++ app/src/main/java/com/brillicode/dvtweatherapp/util/NetworkUtil.kt
@@ -0,0 +1,29 @@
+package com.brillicode.dvtweatherapp.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object NetworkUtil {
+    fun isNetworkAvailable(context: Context): Boolean {
+        // It answers the queries about the state of network connectivity.
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            // Returns details about the currently active default data network.
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+    }
+}

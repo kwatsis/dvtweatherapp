2fd8b8279e92e9253a23f82a9f619abdbcb98db3
ksesotlo@ebay.com <ksesotlo@ebay.com>
2022-01-12 13:32:43 +0200

    Commit unversioned class

---
+++ app/src/main/java/com/brillicode/dvtweatherapp/util/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.brillicode.dvtweatherapp.util
+
+object AppConstants {
+    const val FINE_LOCATION_RC = 2
+    const val TAG = "APP_TAG"
+    const val TIMEOUT: Long = 3000
+}
---
+++ app/src/main/java/com/brillicode/dvtweatherapp/util/network/NetworkUtil.kt
@@ -0,0 +1,29 @@
+package com.brillicode.dvtweatherapp.util.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object NetworkUtil {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+
+        val activeNetwork = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            connectivityManager.activeNetwork ?: return false
+        } else {
+            TODO("VERSION.SDK_INT < M")
+        }
+        val networkCapabilities =
+            connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+
+    }
+}
